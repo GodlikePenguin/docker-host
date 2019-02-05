3ca4e48d8f3015943f6dd50ff5913dc1885e29f4
Edward Dobson <edward.dobson3@ntlworld.com>
2019-02-05 14:26:35 +0000

    Don't use host.docker.internal

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,8 @@ set -e
 if [ $DOCKER_HOST ]; then
   echo "Docker Host: $DOCKER_HOST (manual override)"
 else
-  DOCKER_HOST="$(getent hosts host.docker.internal | cut -d' ' -f1)"
-  if [ $DOCKER_HOST ]; then
-    echo "Docker Host: $DOCKER_HOST (host.docker.internal)"
-  else
-    DOCKER_HOST=$(ip -4 route show default | cut -d' ' -f3)
-    echo "Docker Host: $DOCKER_HOST (default gateway)"
-  fi
+  DOCKER_HOST=$(ip -4 route show default | cut -d' ' -f3)
+  echo "Docker Host: $DOCKER_HOST (default gateway)"
 fi
 
 FORWARDING_PORTS=${PORTS:-'0:65535'}
